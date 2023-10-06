54c554db1d020e19225a0102fdd814dd363f5bf0
DongHyun Kim <temeddix@gmail.com>
2023-10-07 01:20:30 +0900

    First working code

---
+++ src/join_handle.rs
@@ -0,0 +1,103 @@
+use futures_channel::oneshot;
+use std::fmt;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+/// An owned permission to join on a task (awaiting its termination).
+///
+/// This can be thought of as the equivalent of
+/// [`std::thread::JoinHandle`] or [`tokio::task::JoinHandle`] for
+/// a task that is executed concurrently.
+///
+/// A `JoinHandle` *detaches* the associated task when it is dropped, which
+/// means that there is no longer any handle to the task, and no way to `join`
+/// on it.
+///
+/// This struct is created by the [crate::spawn] and [crate::spawn_blocking]
+/// functions.
+///
+/// # Examples
+///
+/// Creation from [`crate::spawn`]:
+///
+/// ```
+/// # async fn doc() {
+/// let join_handle: async_wasm_task::JoinHandle<_> = async_wasm_task::spawn(async {
+///     // some work here
+/// });
+/// # }
+/// ```
+///
+/// Creation from [`crate::spawn_blocking`]:
+///
+/// ```
+/// # async fn doc() {
+/// let join_handle: async_wasm_task::JoinHandle<_> = async_wasm_task::spawn_blocking(|| {
+///     // some blocking work here
+/// });
+/// # }
+/// ```
+///
+/// Child being detached and outliving its parent:
+///
+/// ```no_run
+/// # async fn main() {
+/// let original_task = async_wasm_task::spawn(async {
+///     let _detached_task = async_wasm_task::spawn(async {
+///         // Here we sleep to make sure that the first task returns before.
+///         // Assume that code takes a few seconds to execute here.
+///         // This will be called, even though the JoinHandle is dropped.
+///         println!("♫ Still alive ♫");
+///     });
+/// });
+///
+/// original_task.await.expect("The task being joined has panicked");
+/// println!("Original task is joined.");
+/// # }
+/// ```
+pub struct JoinHandle<T> {
+    receiver: oneshot::Receiver<T>,
+}
+
+unsafe impl<T: Send> Send for JoinHandle<T> {}
+unsafe impl<T: Send> Sync for JoinHandle<T> {}
+impl<T> Unpin for JoinHandle<T> {}
+
+impl<T> JoinHandle<T> {
+    pub(super) fn new(receiver: oneshot::Receiver<T>) -> JoinHandle<T> {
+        JoinHandle { receiver }
+    }
+}
+
+impl<T> Future for JoinHandle<T> {
+    type Output = Result<T, JoinError>;
+    fn poll(mut self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Self::Output> {
+        if let Ok(received) = self.receiver.try_recv() {
+            if let Some(payload) = received {
+                Poll::Ready(Ok(payload))
+            } else {
+                let waker = context.waker().clone();
+                let wake_future = async move {
+                    waker.wake();
+                };
+                wasm_bindgen_futures::spawn_local(wake_future);
+                Poll::Pending
+            }
+        } else {
+            Poll::Ready(Err(JoinError))
+        }
+    }
+}
+
+impl<T> fmt::Debug for JoinHandle<T>
+where
+    T: fmt::Debug,
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("JoinHandle").finish()
+    }
+}
+
+#[derive(Debug)]
+pub struct JoinError;
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,227 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use futures_channel::oneshot;
+pub use join_handle::*;
+use pool::*;
+use std::cell::RefCell;
+use wasm_bindgen::prelude::*;
+
+mod join_handle;
+mod pool;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_js_value(x: &JsValue);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+macro_rules! console_log {
+    ($($t:tt)*) => (crate::log(&format_args!($($t)*).to_string()))
+}
+pub(crate) use console_log;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+thread_local! {
+    pub static WEB_WORKER_POOL: RefCell<WorkerPool> =
+        RefCell::new(WorkerPool::new(4).unwrap());
+}
+
+/// Spawns a new asynchronous task, returning a
+/// [`JoinHandle`] for it.
+///
+/// The provided future will start running in the background immediately
+/// when `spawn` is called, even if you don't await the returned
+/// `JoinHandle`.
+///
+/// Spawning a task enables the task to execute concurrently to other tasks. The
+/// spawned task will always execute on the current thread,
+/// as that's how JavaScript's `Promise` basically works.
+///
+/// # Examples
+///
+/// In this example, a server is started and `spawn` is used to start a new task
+/// that processes each received connection.
+///
+/// ```no_run
+/// use std::io;
+///
+/// async fn process() -> io::Result<()> {
+///     // Some process...
+/// }
+///
+/// async fn start() -> io::Result<()> {
+///     let result = async_wasm_task::spawn(async move {
+///         // Process this job concurrently.
+///         process(socket).await
+///     }).await?;;
+/// }
+/// ```
+///
+/// To run multiple tasks in parallel and receive their results, join
+/// handles can be stored in a vector.
+/// ```
+/// # async fn start() {
+/// async fn my_background_op(id: i32) -> String {
+///     let s = format!("Starting background task {}.", id);
+///     println!("{}", s);
+///     s
+/// }
+///
+/// let ops = vec![1, 2, 3];
+/// let mut tasks = Vec::with_capacity(ops.len());
+/// for op in ops {
+///     // This call will make them start running in the background
+///     // immediately.
+///     tasks.push(async_wasm_task::spawn(my_background_op(op)));
+/// }
+///
+/// let mut outputs = Vec::with_capacity(tasks.len());
+/// for task in tasks {
+///     outputs.push(task.await.unwrap());
+/// }
+/// println!("{:?}", outputs);
+/// # }
+/// ```
+/// This example pushes the tasks to `outputs` in the order they were
+/// started in.
+///
+/// # Using `!Send` values from a task
+///
+/// The task supplied to `spawn` is not required to implement `Send`.
+/// This is different from multi-threaded native async runtimes,
+/// because JavaScript environment is inherently single-threaded.
+///
+/// For example, this will work:
+///
+/// ```
+/// use std::rc::Rc;
+///
+/// fn use_rc(rc: Rc<()>) {
+///     // Do stuff w/ rc
+/// # drop(rc);
+/// }
+///
+/// async fn start() {
+///     async_wasm_task::spawn(async {
+///         // Force the `Rc` to stay in a scope with no `.await`
+///         {
+///             let rc = Rc::new(());
+///             use_rc(rc.clone());
+///         }
+///
+///         async_wasm_task::yield_now().await;
+///     }).await.unwrap();
+/// }
+/// ```
+///
+/// This will work too, unlike multi-threaded native runtimes
+/// where `!Send` values cannot live across `.await`:
+///
+/// ```
+/// use std::rc::Rc;
+///
+/// fn use_rc(rc: Rc<()>) {
+///     // Do stuff w/ rc
+/// # drop(rc);
+/// }
+///]
+/// async fn start() {
+///     async_wasm_task::spawn(async {
+///         let rc = Rc::new(());
+///
+///         async_wasm_task::yield_now().await;
+///
+///         use_rc(rc.clone());
+///     }).await.unwrap();
+/// }
+/// ```
+pub fn spawn<F, T>(future: F) -> JoinHandle<T>
+where
+    F: std::future::Future<Output = T> + 'static,
+    T: 'static,
+{
+    let (sender, receiver) = oneshot::channel();
+    wasm_bindgen_futures::spawn_local(async move {
+        let output = future.await;
+        drop(sender.send(output));
+    });
+    JoinHandle::new(receiver)
+}
+
+/// Runs the provided closure on a web worker(thread) where blocking is acceptable.
+///
+/// In general, issuing a blocking call or performing a lot of compute in a
+/// future without yielding is problematic, as it may prevent the JavaScript runtime from
+/// driving other futures forward. This function runs the provided closure on a
+/// web worker dedicated to blocking operations.
+///
+/// More and more web workers will be spawned when they are requested through this
+/// function until the upper limit of 512 is reached.
+/// After reaching the upper limit, the tasks wait for
+/// any of the web workers to becomes idle.
+/// The web worker limit is very large by default, because `spawn_blocking` is often
+/// used for various kinds of IO operations that cannot be performed
+/// asynchronously.  When you run CPU-bound code using `spawn_blocking`, you
+/// should keep this large upper limit in mind.
+///
+/// This function is intended for non-async operations that eventually finish on
+/// their own. Because web workers do not share memory like threads do,
+/// synchronization primitives such as mutex, channels, and global static variables
+/// might not work as expected.
+///
+/// # Examples
+///
+/// Pass an input value and receive result of computation:
+///
+/// ```
+/// # async fn docs() -> Result<(), Box<dyn std::error::Error>>{
+/// // Initial input
+/// let mut data = "Hello, ".to_string();
+/// let output = async_wasm_task::spawn_blocking(move || {
+///     // Stand-in for compute-heavy work or using synchronous APIs
+///     data.push_str("world");
+///     // Pass ownership of the value back to the asynchronous context
+///     data
+/// }).await?;
+///
+/// // `output` is the value returned from the thread
+/// assert_eq!(output.as_str(), "Hello, world");
+/// Ok(())
+/// }
+/// ```
+pub fn spawn_blocking<C, T>(callable: C) -> JoinHandle<T>
+where
+    C: FnOnce() -> T + Send + 'static,
+    T: Send + 'static,
+{
+    let (sender, receiver) = oneshot::channel();
+    WEB_WORKER_POOL.with(|inner| {
+        let web_worker_pool = inner.borrow();
+        web_worker_pool
+            .run(move || {
+                let output = callable();
+                drop(sender.send(output));
+            })
+            .expect("Unable to run the callable in the worker pool.");
+    });
+    JoinHandle::new(receiver)
+}
+
+/// Yields execution back to the JavaScript event loop.
+///
+/// To avoid blocking inside a long-running function,
+/// you have to yield to the async event loop regularly.
+///
+/// The async task may resume when it has its turn back.
+/// Meahwhile, any other pending tasks will be scheduled.
+pub async fn yield_now() {
+    use wasm_bindgen::prelude::*;
+    #[wasm_bindgen]
+    extern "C" {
+        #[wasm_bindgen(js_name = queueMicrotask)]
+        fn queue_microtask(callback: &js_sys::Function);
     }
+    let promise = js_sys::Promise::new(&mut |resolve, _reject| {
+        queue_microtask(&resolve);
+    });
+    let _ = wasm_bindgen_futures::JsFuture::from(promise).await;
 }
