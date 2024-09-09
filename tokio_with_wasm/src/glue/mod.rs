ae2bc84a2f69d2ef70cecf42cb0c49a84f2c6321
Donghyun Kim <temeddix@gmail.com>
2024-09-09 23:58:37 +0900

    add `rt-multi-thread` feature

--- tokio_with_wasm/src/glue/mod.rs
+++ tokio_with_wasm/src/glue/mod.rs
@@ -5,19 +5,19 @@ mod common;
 #[cfg(feature = "macros")]
 pub use tokio::{join, pin, select, try_join};
 
+#[cfg(feature = "sync")]
+pub use tokio::sync;
+
+#[cfg(feature = "time")]
+pub mod time;
+
 #[cfg(feature = "rt")]
 pub mod task;
 #[cfg(feature = "rt")]
 pub use task::spawn;
 
-#[cfg(all(feature = "rt", feature = "macros"))]
+#[cfg(all(any(feature = "rt", feature = "rt-multi-thread"), feature = "macros"))]
 pub use tokio_with_wasm_proc::main;
 #[doc(hidden)]
-#[cfg(all(feature = "rt", feature = "macros"))]
+#[cfg(all(any(feature = "rt", feature = "rt-multi-thread"), feature = "macros"))]
 pub use wasm_bindgen_futures::spawn_local;
-
-#[cfg(feature = "time")]
-pub mod time;
-
-#[cfg(feature = "sync")]
-pub use tokio::sync;
