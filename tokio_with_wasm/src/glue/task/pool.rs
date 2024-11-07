cfdb650e255552034766ae6b210b763526291607
Slixe <slixeprivate@gmail.com>
2024-11-07 11:09:11 +0100

    remove wasm_bindgen on WorkerPool

--- tokio_with_wasm/src/glue/task/pool.rs
+++ tokio_with_wasm/src/glue/task/pool.rs
@@ -11,7 +11,6 @@ use web_sys::{
 
 pub static MAX_WORKERS: usize = 512;
 
-#[cfg(feature = "workerpool-bindgen", wasm_bindgen)]
 pub struct WorkerPool {
     pool_state: Rc<PoolState>,
 }
@@ -47,7 +46,6 @@ impl Default for WorkerPool {
     }
 }
 
-#[cfg(feature = "workerpool-bindgen", wasm_bindgen)]
 impl WorkerPool {
     /// Creates a new `WorkerPool` which immediately creates `initial` workers.
     ///
@@ -59,7 +57,6 @@ impl WorkerPool {
     ///
     /// Returns any error that may happen while a JS web worker is created and a
     /// message is sent to it.
-    #[cfg(feature = "workerpool-bindgen", wasm_bindgen(constructor))]
     pub fn new() -> WorkerPool {
         WorkerPool::default()
     }
