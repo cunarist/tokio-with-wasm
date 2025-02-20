488f6ebc05fa55437fd0b374f7b5dbe725307fe7
Donghyun Kim <temeddix@gmail.com>
2025-02-20 22:29:55 +0900

    Add comments

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -4,6 +4,8 @@ mod output;
 use entry::async_main;
 use wasm_bindgen::prelude::wasm_bindgen;
 
+// On the web, this macro tells `wasm_bindgen`
+// to run the function on JavaScript module initialization.
 #[wasm_bindgen(start)]
 fn main() {
     async_main();
--- example/src/main.rs
+++ example/src/main.rs
@@ -3,6 +3,8 @@ mod output;
 
 use entry::async_main;
 
+// On native platforms, the `main` function
+// is called by the executable automatically.
 fn main() {
     async_main();
 }
