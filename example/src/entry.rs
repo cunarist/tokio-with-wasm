e34fdf30524970521982735f38457910d362770f
Donghyun Kim <temeddix@gmail.com>
2025-02-20 14:07:13 +0900

    Make the examples more clear

--- example/src/entry.rs
+++ example/src/entry.rs
@@ -7,25 +7,24 @@ use tokio_with_wasm::alias as tokio;
 
 #[tokio::main(flavor = "current_thread")]
 pub async fn async_main() {
+    print_fit!("Tasks spawned.");
     let async_join_handle = spawn(async {
         // Asynchronous code here.
         // This will run concurrently
         // in the same web worker(thread).
-        print_fit!("Async task started.");
         // Simulate a 2-second async task.
         sleep(Duration::from_secs(2)).await;
-        print_fit!("Async task finished.");
     });
-    let _async_result = async_join_handle.await;
-
-    print_fit!("Blocking task spawned.");
     let blocking_join_handle = spawn_blocking(|| {
         // Blocking code here.
         // This will run parallelly
         // in the external pool of web workers.
-        // Simulate a 2-second blocking task.
-        std::thread::sleep(Duration::from_secs(2));
+        // Simulate a 3-second blocking task.
+        std::thread::sleep(Duration::from_secs(3));
     });
+
+    let _async_result = async_join_handle.await;
+    print_fit!("Async task joined.");
     let _blocking_result = blocking_join_handle.await;
     print_fit!("Blocking task joined.");
 
