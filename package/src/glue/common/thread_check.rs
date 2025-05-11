c4b9f3ecc28c808b679ce965fca50779bd6ab454
Danny Kim <temeddix@gmail.com>
2025-05-11 19:36:38 +0900

    Improve the performance of `is_main_thread`

--- package/src/glue/common/thread_check.rs
+++ package/src/glue/common/thread_check.rs
@@ -1,8 +1,15 @@
+use std::cell::LazyCell;
 use web_sys::window;
 
-/// Checks if the current thread is the main JS thread,
-/// not a web worker.
+thread_local! {
+  pub static IS_MAIN_THREAD: LazyCell<bool> =
+    LazyCell::new(|| window().is_some());
+}
+
 pub fn is_main_thread() -> bool {
-  // Check if we have access to the window object.
-  window().is_some()
+  let mut is_main: bool = false;
+  IS_MAIN_THREAD.with(|cell| {
+    is_main = **cell;
+  });
+  is_main
 }
