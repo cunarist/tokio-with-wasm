253d3fb7dc36633cffca20044bb8d78ae7dbdeda
Danny Kim <temeddix@gmail.com>
2026-01-29 23:19:12 +0900

    Format code

--- package/src/glue/only_web/path_provider.rs
+++ package/src/glue/only_web/path_provider.rs
@@ -16,21 +16,21 @@ thread_local! {
 /// JavaScript glue code that bootstraps the wasm module in each worker.
 /// By default the path provider uses a stack trace to determine the path
 /// to the current script.
-/// 
+///
 /// # Example
 /// ```rust,no_run
 /// use tokio_with_wasm::only_web::set_path_provider;
 /// use wasm_bindgen::JsValue;
-/// 
+///
 /// set_path_provider(|| {
 ///     Ok(String::from("/custom/path/to/worker.js"))
 /// });
 /// ```
 #[inline(always)]
 pub fn set_path_provider(provider: fn() -> Result<String, JsValue>) {
-    PATH_PROVIDER.with(|p| {
-        *p.borrow_mut() = provider;
-    });
+  PATH_PROVIDER.with(|p| {
+    *p.borrow_mut() = provider;
+  });
 }
 
 /// Determines the path to the currently executing script by throwing an
@@ -38,15 +38,15 @@ pub fn set_path_provider(provider: fn() -> Result<String, JsValue>) {
 pub fn get_script_path() -> Result<String, JsValue> {
   let string = eval(
     r"
-        (() => {
-            try {
-                throw new Error();
-            } catch (e) {
-                let parts = e.stack.match(/(?:\(|@)(\S+):\d+:\d+/);
-                return parts[1];
-            }
-        })()
-        ",
+      (() => {
+        try {
+          throw new Error();
+        } catch (e) {
+          let parts = e.stack.match(/(?:\(|@)(\S+):\d+:\d+/);
+          return parts[1];
+        }
+      })()
+    ",
   )?
   .as_string()
   .ok_or(JsValue::from(
